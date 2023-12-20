98fc8218b8ccc2f05bab950099059c58c2fc2881
abhimanyu840 <87932372+abhimanyu840@users.noreply.github.com>
2023-12-20 16:49:22 +0530

    fix commit 2

--- pages/login.js
+++ pages/login.js
@@ -119,7 +119,7 @@ const Login = () => {
                                             Login
                                         </button>
                                         <p className="text-sm font-semibold mt-2 pt-1 mb-0">
-                                            Don't have an account?
+                                            Do not have an account?
                                             <span className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"><Link
                                                 href="/signup"
                                             >Register</Link></span>
